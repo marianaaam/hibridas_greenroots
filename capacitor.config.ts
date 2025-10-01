5831e7cff2ab0c0a108fedc621d7c3f068d80eea
Mariana Marin <mariana@Marianas-MacBook-Pro.local>
2025-09-30 19:48:50 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proyecto_greenroots',
+  webDir: 'www'
+};
+
+export default config;
